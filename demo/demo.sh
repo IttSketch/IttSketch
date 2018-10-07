f8934dc60dff91fa788efa3acd668159996b4224
IttSketch <43850833+IttSketch@users.noreply.github.com>
2018-10-07 23:17:56 +0800

    Add files via upload

---
+++ demo/demo.sh
@@ -0,0 +1,20 @@
+cd ../src/Frequency
+make clean
+make
+./main
+make clean
+cd ../HeavyChange
+make clean
+make
+./main
+make clean
+cd ../Persistent
+make clean
+make
+./main
+make clean
+cd ../SuperSpreader
+make clean
+make
+./main
+make clean
